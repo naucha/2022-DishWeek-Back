15bb982fac1d5f866fe4547da9a783add5e3185c
Ignasi Reixach <ignasireixach@gmail.com>
2022-05-30 19:53:08 +0200

    Errors middlewares, notFound error and generalError

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { default: helmet } = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
+const { notFoundError, generalError } = require("./middlewares/errors");
 
 const corsOptions = {
   origin: [
@@ -19,5 +20,8 @@ app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(helmet());
+
+app.use(notFoundError);
+app.use(generalError);
 
 module.exports = app;
