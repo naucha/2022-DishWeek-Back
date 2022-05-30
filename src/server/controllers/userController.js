27f39fdcc83bea5ff5acf4cb43d1f820ec6296ce
Ignasi Reixach <ignasireixach@gmail.com>
2022-05-30 21:09:01 +0200

    Created userRegister controller

---
+++ src/server/controllers/userController.js
@@ -0,0 +1,32 @@
+const bcrypt = require("bcrypt");
+const User = require("../../database/models/User");
+
+const registerUser = async (req, res, next) => {
+  const { name, username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      const encryptedPassword = bcrypt.hash(password, 10);
+      const newUser = {
+        name,
+        username,
+        password: encryptedPassword,
+      };
+
+      await User.create(newUser);
+
+      res.status(201).json({ user: name });
+    } else {
+      const userError = new Error();
+      userError.customMessage = "User name already exist";
+      userError.statusCode = 409;
+
+      next(userError);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { registerUser };
