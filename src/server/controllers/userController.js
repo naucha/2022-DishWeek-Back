c3115e5ccb49aff23dfa39e1cf209ed735737607
Ignasi Reixach <ignasireixach@gmail.com>
2022-05-31 10:41:06 +0200

    Test routerRegister, Fix userController, await in promise

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -1,30 +1,27 @@
+require("dotenv").config();
 const bcrypt = require("bcrypt");
 const User = require("../../database/models/User");
 
 const registerUser = async (req, res, next) => {
   const { name, username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (user) {
+    const userError = new Error();
+    userError.statusCode = 409;
+    userError.customMessage = "User name already exist";
+
+    next(userError);
+    return;
+  }
 
   try {
-    const user = await User.findOne({ username });
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const newUser = { name, username, password: encryptedPassword };
 
-    if (!user) {
-      const encryptedPassword = bcrypt.hash(password, 10);
-      const newUser = {
-        name,
-        username,
-        password: encryptedPassword,
-      };
+    await User.create(newUser);
 
-      await User.create(newUser);
-
-      res.status(201).json({ name });
-    } else {
-      const userError = new Error();
-      userError.statusCode = 409;
-      userError.customMessage = "User name already exist";
-
-      next(userError);
-    }
+    res.status(201).json({ username });
   } catch (error) {
     next(error);
   }
---
+++ src/server/routers/userRouter.test.js
@@ -0,0 +1,62 @@
+const request = require("supertest");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const mongoose = require("mongoose");
+
+const connectDB = require("../../database");
+const User = require("../../database/models/User");
+const app = require("..");
+const mockUser = require("../mocks/mockUser");
+
+let mongoServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  await connectDB(mongoServer.getUri());
+});
+
+beforeEach(async () => {
+  await User.create(mockUser[0]);
+});
+
+afterEach(async () => {
+  await User.deleteMany({});
+});
+
+afterAll(async () => {
+  await mongoServer.stop();
+  await mongoose.connection.close();
+});
+
+describe("Given a POST '/register' endpoint", () => {
+  describe("When it receives a request", () => {
+    test("Then it should receive a object with a new created user", async () => {
+      const newUserData = {
+        name: "Pepito",
+        username: "Grillo82",
+        password: "0000",
+      };
+
+      const response = await request(app)
+        .post("/user/register")
+        .send(newUserData)
+        .expect(201);
+
+      expect(response.body.id).not.toBeNull();
+      expect(response.body.username).toBe("Grillo82");
+    });
+  });
+
+  describe("When it receives a request with an existing user", () => {
+    test("Then it should call the response method status code 409", async () => {
+      const newUserData = {
+        name: "Manolito",
+        username: "Gafotas23",
+        password: "0000",
+      };
+
+      await User.create(newUserData);
+
+      await request(app).post("/user/register").send(newUserData).expect(409);
+    });
+  });
+});
