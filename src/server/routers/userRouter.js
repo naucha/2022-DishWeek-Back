572b78500ade6cf1982d2ba8bb666943184c2ce1
Ignasi Reixach <ignasireixach@gmail.com>
2022-06-01 18:41:42 +0200

    Add the userRouter path

--- src/server/routers/userRouter.js
+++ src/server/routers/userRouter.js
@@ -4,10 +4,11 @@ const { validate } = require("express-validation");
 const { registerUser } = require("../controllers/userController");
 const {
   credentialsRegisterSchema,
+  credentialsLoginSchema,
 } = require("../schemas/userCredentialsSchema");
 
 const userRouter = express.Router();
 
 userRouter.post("/register", validate(credentialsRegisterSchema), registerUser);
-
+userRouter.post("/login", validate(credentialsLoginSchema), "");
 module.exports = userRouter;
