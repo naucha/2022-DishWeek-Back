4e5c40933035c9896bfc9e23535c100e6ee7c28a
Ignasi Reixach <ignasireixach@gmail.com>
2022-06-01 19:20:05 +0200

    Add login controller and json endpoints postman file

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const bcrypt = require("bcrypt");
+const jsonwebtoken = require("jsonwebtoken");
 const User = require("../../database/models/User");
+const { customError } = require("../../utils/customError");
 
 const registerUser = async (req, res, next) => {
   const { name, username, password } = req.body;
@@ -27,4 +29,32 @@ const registerUser = async (req, res, next) => {
   }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    const error = customError(403, "Bad request", "User or password not valid");
+
+    next(error);
+    return;
+  }
+  const userData = {
+    username: user.username,
+    name: user.name,
+  };
+
+  const rightPassword = await bcrypt.compare(password, user.password);
+  if (!rightPassword) {
+    const error = customError(403, "Bad request", "User or password not valid");
+
+    next(error);
+    return;
+  }
+  const token = jsonwebtoken.sign(userData, process.env.JWT_SECRET);
+
+  res.status(200).json({ token });
+};
+
+module.exports = { registerUser, LoginUser: loginUser };
--- src/server/routers/userRouter.js
+++ src/server/routers/userRouter.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const { validate } = require("express-validation");
-const { registerUser } = require("../controllers/userController");
+const { registerUser, LoginUser } = require("../controllers/userController");
 const {
   credentialsRegisterSchema,
   credentialsLoginSchema,
@@ -10,5 +10,5 @@ const {
 const userRouter = express.Router();
 
 userRouter.post("/register", validate(credentialsRegisterSchema), registerUser);
-userRouter.post("/login", validate(credentialsLoginSchema), "");
+userRouter.post("/login", validate(credentialsLoginSchema), LoginUser);
 module.exports = userRouter;
